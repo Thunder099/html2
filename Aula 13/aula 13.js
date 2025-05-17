3ac13d86530555e473e9a60a594c8e929bdb8237
Thundere <44729071+Thunder099@users.noreply.github.com>
2025-05-17 15:57:41 -0300

    Add files via upload

---
+++ Aula 13/aula 13.js
@@ -0,0 +1,15 @@
+// let num1 = 10
+// let num2 = 20
+// let num3 = 30
+
+// let soma1 = num1 + num2
+// let soma2 = num2 + num3
+// let soma3 = num1 + num3
+
+// alert(soma1)
+// alert(soma2)
+// alert(soma3)
+
+let ah = [55,10,"cavalos"]
+
+console.log(ah[1], ah[2])
