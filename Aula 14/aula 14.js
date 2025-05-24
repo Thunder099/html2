88e8317754401a02eccf3aad72571d208007d771
Thundere <44729071+Thunder099@users.noreply.github.com>
2025-05-24 16:04:06 -0300

    Add files via upload

---
+++ Aula 14/aula 14.js
@@ -0,0 +1,67 @@
+// let num1 = 10
+// let num2 = 20
+
+// if (num1 == num2) {
+//     alert("É igual")
+
+// } else {
+//     alert("É diferente")
+// }
+
+
+
+
+
+// Crie um programa, contendo duas variáveis, e mostre em um alert, qual é maior e qual é menor.
+
+// let CAVALO = 100
+// let carrasco = 100
+
+// if (CAVALO > carrasco) {
+
+//     console.log(CAVALO + " É maior que " + carrasco)
+
+// } else if (carrasco > CAVALO) {
+
+//     console.log(carrasco + " É maior que " + CAVALO)
+
+// } else {
+
+//     console.log(CAVALO + " e " + carrasco + " São iguais")
+
+// }
+
+
+
+// for - while => laço de repetiçao
+
+// let controle = 0
+
+// while (controle < 5) {
+
+//     alert(controle)
+
+//     controle++
+// }
+
+
+// for(let ctrl = 0; ctrl < 5; ctrl++) {
+//     console.log(ctrl)
+// }
+
+
+// Crie um programa, onde contenha uma variável, e verifique em um looping se essa variável é igual ao controle, 
+
+let v1 = 50
+let controle = 0
+
+while (controle < v1) {
+
+    console.log(controle)
+    controle++
+
+} if (controle == v1) {
+
+    console.log("controle é igual a v1")
+
+}
