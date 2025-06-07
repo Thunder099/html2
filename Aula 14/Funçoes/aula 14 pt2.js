6b68129c787512602814a18a47ee0591a8198e0c
Thundere <44729071+Thunder099@users.noreply.github.com>
2025-06-07 16:03:54 -0300

    Add files via upload

---
+++ Aula 14/Funçoes/aula 14 pt2.js
@@ -0,0 +1,11 @@
+function teste() {
+
+    // let h1 = document.getElementById("text").textContent
+
+    // let uva = document.getElementById("text").innerHTML = "Bom dia"
+
+    let oi = document.getElementsByClassName("ativar")[0].textContent
+
+    alert(oi)
+
+}
