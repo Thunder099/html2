29cb78861fe04f746eb9c3dddddfa4d9c3458d3d
Thundere <44729071+Thunder099@users.noreply.github.com>
2025-06-28 15:56:30 -0300

    Add files via upload

---
+++ Aula 15/aula 15.js
@@ -0,0 +1,24 @@
+let link_1 = "https://youtube.com"
+let link_2 = "https://github.com/"
+let link_3 = "https://www.instagram.com/accounts/login/"
+let link_4 = "https://youtube.com"
+
+function direcionar(num_link) {
+
+    if (num_link == 1){
+        window.location.href = link_1
+    }
+
+    if (num_link == 2){
+        window.location.href = link_2
+    }
+
+    if (num_link == 3){
+        window.location.href = link_3
+    }
+
+    if (num_link == 4){
+        window.location.href = link_4
+    }
+
+}
