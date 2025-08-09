284cf7f4d7b239765738f259154dabad5a6d0ce7
Thundere <44729071+Thunder099@users.noreply.github.com>
2025-08-09 16:03:21 -0300

    Add files via upload

---
+++ Segundo bimestre/Aula 1/aula 1.js
@@ -0,0 +1,56 @@
+// let teste1 = "bom dia"
+
+// console.log(teste1)
+
+function tarde() {
+
+    teste1 = "boa tarde"
+    console.log(teste1)
+
+}
+
+let botao = document.getElementById=(oi)
+
+botao.addEventListener("click",tarde)
+
+
+
+// for(let controle = 0; controle < 10; controle++){
+//     console.log("CAVALO")
+// }
+
+// verifica informação antes de rodar
+// let batata = 0
+// while (batata < 10) {
+
+//     console.log("BATATA")
+//     batata++
+
+// }
+
+// mostra informação antes de verificar
+// let mouses = 0
+// do {
+//     console.log("mouses")
+
+//     mouses++
+// } while(mouses < 10);
+
+
+// let cavalo = 0
+
+// if (cavalo == 0) {
+//     console.log("é zero")
+
+
+// }else if (cavalo == 10) {
+
+//    console.log("é deix")
+
+// }else {
+
+//     console.log("num sei")
+
+// }
+
+
